72aed8809c1b955318dd938f9af08e9c307fc572
Christian Edwards (s5109267) <s5109267@mac38566.mac.bournemouth.ac.uk>
2019-04-02 12:18:44 +0100

    original project

--- Group project/GameViewController.swift
+++ Group project/GameViewController.swift
@@ -1,11 +1,3 @@
-//
-//  GameViewController.swift
-//  Group project
-//
-//  Created by Christian Edwards (s5109267) on 02/04/2019.
-//  Copyright © 2019 Christian Edwards (s5109267). All rights reserved.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
