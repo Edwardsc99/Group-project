3806ae0234fd8d1b20f501bb8883b5d6fc4fa23b
Liam Driver (s5111108) <s5111108@mac38567.mac.bournemouth.ac.uk>
2019-05-07 12:15:06 +0100

    kyle help

--- Group project/GameScene.swift
+++ Group project/GameScene.swift
@@ -10,12 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var yellow: SKSpriteNode!
-    var blue :SKSpriteNode!
-    var red: SKSpriteNode!
-    var green: SKSpriteNode!
     
-    let ball = SKSpriteNode(imageNamed: "ball")
+    var force = -20.0
+    let target = SKSpriteNode(imageNamed: "ColorCircle")
     
     override func didMove(to view: SKView) {
         layoutScene()
@@ -23,36 +20,53 @@ class GameScene: SKScene {
     
     func layoutScene() {
         backgroundColor = UIColor.black
-        
-        
-        
-        
         spawnball()
+        target.size = CGSize(width: 120.0, height: 120.0)
+        target .position = CGPoint(x: frame.midX, y: 150)
+        target.physicsBody = SKPhysicsBody(circleOfRadius: target.size.width/2)
+        target.physicsBody?.isDynamic = false
+        addChild(target)
     }
     
     func spawnball() {
+        let ball = SKSpriteNode(imageNamed: "ball")
         ball.size = CGSize(width: 30.0, height: 30.0)
-        ball .position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.affectedByGravity = true
+        physicsWorld.gravity = CGVector(dx: 0, dy: -2.0)
         addChild(ball)
+        ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: force))
+        
     }
     
-    func spriteSetup(){
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        yellow = SKSpriteNode()
-        yellow.size = CGSize(width: frame.size.width/3, height: frame.size.height/3)
-        addChild(yellow)
         
-        red = SKSpriteNode()
-        red.size = CGSize(width: frame.size.width/3, height: frame.size.height/3)
-        addChild(red)
-        
-        blue = SKSpriteNode()
-        blue.size = CGSize(width: frame.size.width/3, height: frame.size.height/3)
-        addChild(blue)
-        
-        green = SKSpriteNode()
-        green.size = CGSize(width: frame.size.width/3, height: frame.size.height/3)
-        addChild(green)
+        force += 0.2
+        let touch = touches.first!
+        let touchLocation = touch.location(in: self)
+
+        if touchLocation.x > frame.midX {
+            target.zRotation += deg2rad(90)
+        } else {
+            target.zRotation -= deg2rad(90)
+        }
         
     }
+    
+    
+    func deg2rad(_ number: CGFloat) -> CGFloat {
+        return number * .pi / 180
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        for child in children {
+            if child.position.y < -100 {
+                child.removeFromParent()
+            }
+        }
+    }
+    
 }
