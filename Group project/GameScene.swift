967a8f2357d37115935bb7a897a42d9ed420c1f9
Liam Driver (s5111108) <s5111108@mac42012.mac.bournemouth.ac.uk>
2019-05-15 10:08:49 +0100

    Finished piece

--- Group project/GameScene.swift
+++ Group project/GameScene.swift
@@ -18,46 +18,45 @@ enum SwitchState: Int {
 
 class GameScene: SKScene {
     
-    let uuid = UUID().uuidString
+    let uuid = UUID().uuidString                                                            // this creates a unique user id for identification of the user
     var switchState = SwitchState.red
     var currentColourIndex: Int?
     var force = -20.0
-    let target = SKSpriteNode(imageNamed: "ColorCircle")
+    let target = SKSpriteNode(imageNamed: "ColorCircle")                                    // this creates a sprite for the circle and uses the image colorcirle for the graphics
     let scoreLabel = SKLabelNode(text: "0")
-    var score = 0
+    var score = 0                                                                           // this is the varable for the score which is tracked through the app
     var blingSound: SKAction?
-    let highscore = 0
     
     override func didMove(to view: SKView) {
-        setupPhysics()
-        layoutScene()
+        setupPhysics()                                                                      // this recalls the physics function
+        layoutScene()                                                                       // this recalls the layout scene function
         self.blingSound = SKAction.playSoundFileNamed("bling", waitForCompletion: false)
     }
     
     func setupPhysics() {
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -1.0)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -1.0)                                  //this sets the gravity to minus 1 so the ball is negativly effected by this and heads down.
   physicsWorld.contactDelegate = self
     }
 
     func save() {
         
-        let scoreRef = Firestore.firestore().collection("scores").document(uuid)
-        scoreRef.setData(["score": score]) { error in }
+        let scoreRef = Firestore.firestore().collection("scores").document(uuid)            // this then sets the score reference to the a firestore in firebase
+        scoreRef.setData(["score": score]) { error in }                                     // this sets the data of the score to be sent to firebase
 
     }
     
     func layoutScene() {
         backgroundColor = UIColor.black
         spawnball()
-        target.size = CGSize(width: 120.0, height: 120.0)
-        target .position = CGPoint(x: frame.midX, y: 150)
-        target.physicsBody = SKPhysicsBody(circleOfRadius: target.size.width/2)
+        target.size = CGSize(width: 120.0, height: 120.0)                                  //this is where the size of the circle is set
+        target .position = CGPoint(x: frame.midX, y: 150)                                   // this is where the position of the circle is
+        target.physicsBody = SKPhysicsBody(circleOfRadius: target.size.width/2)             // this sets the physics body to be half the size of the circle
         target.zPosition = ZPositions.target
         target.physicsBody?.categoryBitMask = physicsCategories.switchcategory
-        target.physicsBody?.isDynamic = false
-        addChild(target)
+        target.physicsBody?.isDynamic = false                                               // this sets it so that gravity doesnt effect the ball
+        addChild(target)                                                                    // this adds the circle to the scene
         
-       scoreLabel.fontName = "AvenirNext-Bold"
+       scoreLabel.fontName = "AvenirNext-Bold"                                              // this creates the score label by setting the font, font size, color, position and adds to the scene
        scoreLabel.fontSize = 60.0
        scoreLabel.fontColor = UIColor.white
        scoreLabel.position = CGPoint(x: frame.midX, y: frame.midY)
@@ -67,13 +66,13 @@ class GameScene: SKScene {
     }
     
     func updateScoreLabel() {
-        scoreLabel.text = "\(score)"
+        scoreLabel.text = "\(score)"                                                    // this function updates the scorelabel with the score
     }
     
     func spawnball() {
-        currentColourIndex = Int(arc4random_uniform((4)))
+        currentColourIndex = Int(arc4random_uniform((4)))                               // this randomly selects a color for the ball
         
-        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColours.colours[currentColourIndex!], size: CGSize(width: 30.0, height: 30.0))
+        let ball = SKSpriteNode(texture: SKTexture(imageNamed: "ball"), color: PlayColours.colours[currentColourIndex!], size: CGSize(width: 30.0, height: 30.0))       //this sets up the ball sprite with size, position and a physicsbody
         ball.colorBlendFactor = 1.0
         ball.name = "Ball"
         ball.position = CGPoint(x: frame.midX, y: frame.maxY)
@@ -93,26 +92,26 @@ class GameScene: SKScene {
             switchState = .red
         }
         
-        target.run(SKAction.rotate(byAngle: .pi/2, duration: 0.25))
+        target.run(SKAction.rotate(byAngle: .pi/2, duration: 0.25))                         // this rotates the wheel 90 degrees anti clockwise
         
     }
     
-    func gameOver() {
+    func gameOver() {                                                                       // this is the gameover/reset function
         run(SKAction.playSoundFileNamed("game_over", waitForCompletion: true)){
-            UserDefaults.standard.set(self.score, forKey: "RecentScore")
+            UserDefaults.standard.set(self.score, forKey: "RecentScore")                    // this sets the recentscore label to the score of the previous game
             if self.score > UserDefaults.standard.integer(forKey: "Highscore"){
-                UserDefaults.standard.set(self.score, forKey: "Highscore")
+                UserDefaults.standard.set(self.score, forKey: "Highscore")                  // this then sets the highscore label if the score just obtained then it changes this label as well
             
         }
         
         let menuScene = MenuScene(size: self.view!.bounds.size)
-        self.view!.presentScene(menuScene)
+        self.view!.presentScene(menuScene)                                                  // this then presents the menu scene
         }
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        turnWheel()
+        turnWheel()                                                                         // this turns the wheel when the user touches the screen
     }
     }
     
@@ -121,18 +120,18 @@ class GameScene: SKScene {
 extension GameScene: SKPhysicsContactDelegate {
     
     func didBegin(_ contact: SKPhysicsContact) {
-        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask             // this recalls the contact masks of both sprites into one function
         
         if contactMask == physicsCategories.ballcategory |
             physicsCategories.switchcategory{
-            if let ball = contact.bodyA.node?.name == "Ball" ?
+            if let ball = contact.bodyA.node?.name == "Ball" ?                                      //this checks the contact is with the ball
                 contact.bodyA.node as? SKSpriteNode : contact.bodyB.node as? SKSpriteNode {
-                if currentColourIndex == switchState.rawValue {
-                    if blingSound == self.blingSound{
+                if currentColourIndex == switchState.rawValue {                                     // this checks if color is the matches with both the ball and the wheel
+                    if blingSound == self.blingSound{                                               // this runs if the color is correct
                         run(blingSound!)
                     }
-                    score += 1
-                    if score > 5 {
+                    score += 1                                                                      // adds one to the score
+                    if score > 5 {                                                                  // this then adds to the gravity if the score of 6,11,21,26 are reached
                         physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
                     }
                     if score > 10 {
@@ -144,15 +143,15 @@ extension GameScene: SKPhysicsContactDelegate {
                     if score > 25 {
                         physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.0)
                     }
-                    updateScoreLabel()
-                    ball.run(SKAction.fadeOut(withDuration: 0.25), completion: { ball.removeFromParent()
-                    self.spawnball()
+                    updateScoreLabel()                                                              // this then updates the score label
+                    ball.run(SKAction.fadeOut(withDuration: 0.25), completion: { ball.removeFromParent()                // removes the ball from the parent and fades it out
+                    self.spawnball()                                                                                    // then spawns another ball
                 
             })
             
                 } else {
-                    save()
-                    gameOver()
+                    save()                                                                                      // otherwise runs the save function
+                    gameOver()                                                                                  // and restarts the add
                     
 }
 
--- Group project/MenuScene.swift
+++ Group project/MenuScene.swift
@@ -12,14 +12,14 @@ import SpriteKit
 class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
-        backgroundColor = UIColor.black
         addLogo()
+        backgroundColor = UIColor.black                                     // this sets the background color to black
         addLabels()
         
     }
 
     func addLogo() {
-        let logo = SKSpriteNode(imageNamed: "Logo")
+        let logo = SKSpriteNode(imageNamed: "ColorCircle")                                      // Logo should appear
         logo.size = CGSize(width: frame.size.width/4, height: frame.size.width/4)
         logo.position = CGPoint(x: frame.midX, y: frame.midY + frame.midY + frame.size.height/4)
         addChild(logo)
@@ -27,7 +27,7 @@ class MenuScene: SKScene {
     }
     func addLabels() {
         
-        let playLabel = SKLabelNode (text: "Tap to Play!")
+        let playLabel = SKLabelNode (text: "Tap to Play!")                                                                                      //Creates the tap to play label
         playLabel.fontName = "AvenirNext-Bold"
         playLabel.fontSize = 50.0
         playLabel.fontColor = UIColor.white
@@ -35,14 +35,14 @@ class MenuScene: SKScene {
         addChild(playLabel)
         animate(label: playLabel)
         
-        let highScoreLabel = SKLabelNode(text: "Highscore: " + "\( UserDefaults.standard.integer(forKey: "Highscore"))")
+        let highScoreLabel = SKLabelNode(text: "Highscore: " + "\( UserDefaults.standard.integer(forKey: "Highscore"))")                        //creates the highscore label
         highScoreLabel.fontName = "AvenirNext-Bold"
         highScoreLabel.fontSize = 40.0
         highScoreLabel.fontColor = UIColor.white
         highScoreLabel.position = CGPoint(x: frame.midX, y: frame.midY - highScoreLabel.frame.size.height*4)
         addChild(highScoreLabel)
         
-        let recentScoreLabel = SKLabelNode (text: "Recent Score: " + "\(UserDefaults.standard.integer(forKey: "RecentScore"))")
+        let recentScoreLabel = SKLabelNode (text: "Recent Score: " + "\(UserDefaults.standard.integer(forKey: "RecentScore"))")                 //creates recent score label
         recentScoreLabel.fontName = "AvenirNext-Bold"
         recentScoreLabel.fontSize = 40.0
         recentScoreLabel.fontColor = UIColor.white
@@ -54,13 +54,13 @@ class MenuScene: SKScene {
 //        let fadeOut = SKAction.fadeOut(withDuration: 0.5)
 //        let fadeIn = SKAction.fadeIn(withDuration: 0.5)
         
-        let scaleUp = SKAction.scale(to: 1.1, duration: 0.5)
+        let scaleUp = SKAction.scale(to: 1.1, duration: 0.5)                                                        //this is to animate the tap to play label and make it pulse
         let scaleDown = SKAction.scale(to: 1.0, duration: 0.5)
         
         let sequence = SKAction.sequence([scaleUp,scaleDown])
         label.run(SKAction.repeatForever(sequence))
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {                                     //to add function of starting the game
         let gameScene = GameScene(size:view!.bounds.size)
         view!.presentScene(gameScene)
         
